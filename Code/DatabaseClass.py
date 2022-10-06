f4a26e5639109b3e2c093e3934ccb1ebd9ad80b8
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2022-10-06 19:55:19 +0100

    Removed code for SQLite
    I have decided to use MySQL instead of SQLite. This is because MySQL will be more realistic for a game as users would be able to connect remotely to a database on a server (which would store their account and stats). The code will be easily modified so that this can happen. For now, the database will be stored locally.

--- Code/DatabaseClass.py
+++ Code/DatabaseClass.py
@@ -1,17 +1,11 @@
-import mysql.connector
+import mysql
 
 class Database:
-    __host = None
-    __username = None
-    __password = None
-
     def __init__(self, host, username, password):
-        self.__host == host
-        self.__username == username
+        self.__host = host
+        self.__username = username
         self.__password = password
 
-        db = mysql.connector.connect(self.__host, self.__username, self.__password)
-        dbCursor = db.cursor() # Executes SQL statements
+        databaseConnection = mysql.connector.connect(self.__host, self.__username, self.__password)
+        databaseCursor = databaseConnection.cursor() # Executes SQL statements
         
-        # Check if database exists. Create it if it doesn't
-        dbCursor.execute("SHOW DATABASES")
--- Code/MainMenu.py
+++ Code/MainMenu.py
@@ -1,4 +1,3 @@
-import sqlite3
 import pygame
 import ButtonClass
 import DatabaseClass
@@ -21,6 +20,7 @@ pygame.init()
 
 window = pygame.display.set_mode((WIDTH,HEIGHT))
 
+# Display Main Menu
 def mainMenuWindow(slotOne, slotTwo):
     running = True
 
@@ -61,12 +61,8 @@ def mainMenuWindow(slotOne, slotTwo):
 slotOne = AccountSlotClass.AccountSlot()
 slotTwo = AccountSlotClass.AccountSlot()
 
-# ----> Database Setup
-databaseConnection = sqlite3.connect("draughtsGameDB.db") # Returns a connection object which is used to interact with the database. If the database doesn't exist, creates it and then connects to it.
-databaseCursor = databaseConnection.cursor() # Returns a cursor object. Used to send SQL statements to the database
+# ----> Database launch
+database = DatabaseClass.Database() # Add host, username, password
 
-databaseCursor.execute("CREATE TABLE IF NOT EXISTS UserAccount(AccountID INTEGER PRIMARY KEY AUTOINCREMENT, Username CHAR(15) NOT NULL, Password CHAR(30) NOT NULL, DateCreated CHAR(10) NOT NULL)")
-databaseCursor.execute("INSERT INTO UserAccount (Username, Password, DateCreated) VALUES ('userOne', 'passwordOne', date('2022-04-04'))")
-databaseConnection.commit()
 # ----> Display the Menu
 mainMenuWindow(slotOne, slotTwo)
